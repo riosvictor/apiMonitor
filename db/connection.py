4cd0b08f5943522eeaa8cf3bc7f47dabe17521b7
Paulo Rios <paulo.pinto@trizy.com.br>
2021-01-16 11:09:39 -0300

    Update connection.py
    atualizando a serie quando receber um novo envio

--- db/connection.py
+++ db/connection.py
@@ -45,14 +45,15 @@ def insert_serie(id_computer, data, type, array_serie):
         'serie': ",".join(map(str, array_serie))
     }
 
-    pc = db.serie
+    serie = db.serie
 
-    serie_search = pc.find_one({'id_computer': id_computer, 'data': data, 'type': type})
+    serie_search = serie.find_one({'id_computer': id_computer, 'data': data, 'type': type})
 
     if serie_search:
         print(serie_search.get('_id'))
+        serie.update_one({'_id': serie_search.get('_id')}, {'$set': {'serie': serie_data.get('serie')}})
         return serie_search.get('_id')
     else:
-        result = pc.insert_one(serie_data)
+        result = serie.insert_one(serie_data)
         print('One post: {0}'.format(result.inserted_id))
         return result.inserted_id
